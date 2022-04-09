d4a8cbb7608393dc1bbab4b070e4c9712a800853
avarela <avarelagm@gmail.com>
2022-04-09 10:35:14 +0100

    first commit

---
+++ archive_scanned_docs.py
@@ -0,0 +1,114 @@
+# she-bang for Python3 scripts:
+#!/usr/bin/env python3
+
+""" 
+ArchiveScannedDocs
+This Module is the main app.
+"""
+
+import os
+import logging
+from tkinter import Y
+from other_files import helper_functions
+
+logging.basicConfig(filename=r'\\ARBACKUP-PC\Arquive\Scanned Docs\Log\jobdone.log',
+                    format='%(asctime)s %(message)s', level=logging.DEBUG)
+
+pc_asserver = r'\\ARBACKUP-PC'
+archive = r'\Arquive'
+pc_asserver_arch = pc_asserver + archive
+scanned_docs_dir = pc_asserver_arch + r'\Scanned Docs'
+tmp_dir = scanned_docs_dir + r'\tmp'
+
+''' for all files in scanned docs directory '''
+for file in os.listdir(scanned_docs_dir):
+
+    # check if is a pdf file
+    if not helper_functions.ispdf(file):
+        continue
+
+    # builds the orc file name adding "ocr_" at the beggining of the original file name
+    ocr_file = "ocr_" + file
+
+    # builds the name including the PATH of the original, scanned or printed file.
+    orig_file = os.path.join(scanned_docs_dir, file)
+
+    # makes the original and ocr the same folder (this way they can be separated any time)
+    ocr_scanned_docs_dir = scanned_docs_dir
+
+    # builds the name including the PATH of the OCR file.
+    ocr_file = os.path.join(ocr_scanned_docs_dir, ocr_file)
+
+    # Creates and returns a PDF/A file, ocr_file, and if the original file is scanned or printed.
+    ocr_file, file_is_scanned = helper_functions.creates_pdfa_file(
+        orig_file, ocr_file)
+
+    # extracts text from pdfa file and returns it assigned to the string read_pdf
+    text_extracted = helper_functions.extracts_text(ocr_file)
+
+    try:
+        # Move original files to tmp folder. These files must be deleted later, after a few days.
+        os.replace(orig_file, os.path.join(tmp_dir, file))
+
+    # if the file is being used by another process.
+    except PermissionError as e:
+        msg = f'Error copying file to tmp folder,\n{e}.\nPlease press ENTER to continue'
+
+        # Prompts the user to prees ENTER after showing an error.
+        helper_functions.prompt_user(msg)
+
+        # log the error
+        logging.exception(f'Error copying file to tmp folder, {e}.')
+
+        # resume to the next document.
+        continue
+
+    # Gets all details of the document to be archived, including a list of all pcos from the emails.
+    doc_ext = helper_functions.get_type_doc_details(text_extracted)
+
+    if not doc_ext is None:
+        # unboxex document details returned
+        doctype, lstofdoc, dest_archive_file_name = doc_ext
+    else:
+        # if doc_ext is empty, inform user there a ocr error and resume to next document.
+        msg = f'ocr error on file {os.path.basename(ocr_file)}.\nPlease press ENTER to continue!'
+
+        # Prompts the user to prees ENTER after showing an error.
+        helper_functions.prompt_user(msg)
+
+        # resume to the next document.
+        continue
+
+    # if the list of pcos is not empty.
+    if not lstofdoc is None:
+        for doc in lstofdoc:
+            nr, pco_year = doc
+            # gets the complete file name, with destination path and the destination file
+            file_name = helper_functions.get_comp_filename(
+                pc_asserver_arch, doctype, nr, pco_year, dest_archive_file_name)
+
+            # unboxex path and file name returned
+            path_to_folder, path_to_file = file_name
+
+            # print(file_name)
+
+            # helper_functions.copy_files_to_folders(lstofdoc, path_to_folder, path_to_file, orig_file_name)
+            helper_functions.copy_files_to_folders(
+                ocr_file, path_to_folder, path_to_file)
+
+            # log the ocr file was just duly filed on the correspondent folder.
+            logging.info(
+                f'The original file: {os.path.basename(path_to_file)} was duly archived.')
+
+        # remove the just copied ocr file from scanned folder
+        os.remove(ocr_file)
+
+        # inform user and log the ocr file was just removed from scanned files
+        logging.info(
+            f'The ocr file file, {os.path.basename(ocr_file)} was just removed from scanned files folder.\n\n *** ========================================================= ***\n\n')
+        print(
+            f'The ocr file file, {os.path.basename(ocr_file)} was just removed from scanned files folder.')
+
+    else:
+        # inform user the email was not valid for archive.
+        print('File not valid for arquive.')
---
+++ other_files/helper_functions.py
@@ -0,0 +1,237 @@
+# helper_functions.py
+
+import os, re, logging
+import shutil
+import ocrmypdf
+from io import StringIO
+from pdfminer.high_level import extract_text_to_fp
+
+ocrmypdf.configure_logging(-1)
+
+''' Composes the all document number '''
+def format_doc_numer(doc, nr, year):
+    # Composes the all document number
+    formated_doc_nr = 'CGO-' + doc + nr + '-' + year
+    return formated_doc_nr
+
+''' Prompts the user to prees ENTER after showing an error. '''
+def prompt_user(msg):
+    inp = ''
+    while inp != 'y':
+        inp = input(msg)
+        inp = 'y'
+
+''' returns details of PEI or ECI document '''
+def prep_peci_data(peci_desc):
+    # Extracts the doc type, *EI or ECI
+    doc = peci_desc[1]
+    
+    # Extracts and fomats the document number(0000)
+    peci_nr = str.zfill((peci_desc[3]).strip(' '), 4)
+    
+    # Extracts the year of the document
+    peci_year = peci_desc[2]
+    
+    # Composes the all document number
+    peci_full_file_name = format_doc_numer(doc, peci_nr, peci_year)
+    
+    # returns a list with the document number and document year
+    lst_pco = [(peci_nr, peci_year)]
+    
+    return doc, lst_pco, peci_full_file_name
+
+
+''' returns details of RI document '''
+def prep_ri_data(ri_desc):
+    # Retuns RI
+    ri_doc = 'RI'
+    
+    # Extracts and fomats the ri document number(0000)
+    ri_nr = str.zfill((ri_desc[1]).strip(' '), 4)
+    
+    # Extracts the year of the ri document
+    ri_year = ri_desc[2]
+    
+    # Composes the all document number
+    ri_full_file_name = format_doc_numer(ri_doc, ri_nr, ri_year)
+            
+    # returns a list with the document number and document year
+    lst_ri = [(ri_nr, ri_year)]
+    
+    return ri_doc, lst_ri, ri_full_file_name
+
+
+''' Returns true if the current file extention is ".pdf", is a pdf file '''
+def ispdf(cur_file):
+    if cur_file.endswith(".pdf"):
+        return True
+
+
+''' Creates and returns a PDF/A file '''
+def creates_pdfa_file(img_file, pdfa_file):
+    
+    # informs if the current file is NOT a scanned file.
+    isscanned=False
+    
+    # checks if the path and the file exists and are accessible.
+    if os.path.isfile(img_file) and os.access(img_file, os.R_OK):
+        try:
+            
+            # if the original file is a scanned image pdf, makes a new PDF/A file, with a text layer that can be extracted.
+            #if __name__ == '__main__':  # To ensure correct behavior on Windows and macOS
+                ocrmypdf.ocr(img_file, pdfa_file, deskew=True, progress_bar=False, language = 'por')
+                
+                # informs if the current file is a scanned file.
+                isscanned = True
+
+        # if the original file is already a PDF/A file, a exception is thrown where we create a orc file
+        # by copying and rename the original original file.
+        except TypeError:
+            
+            logging.exception(f'The file {img_file} is not a scanned file!')
+            # not an image file, already a PDF/A file
+            shutil.copy(img_file, pdfa_file)
+
+    return pdfa_file, isscanned
+
+
+''' extracts text from pdfa file and returns it assigned to the string read_pdf '''
+def extracts_text(pdfa_file):
+    # Extracts text from pdfa file
+    try:
+        output_string = StringIO() 
+        with open(pdfa_file, 'rb') as read_pdf:
+            extract_text_to_fp(read_pdf, output_string)
+
+        # returns the assigned text extracted from pdfa file to the string read_pdf
+        read_pdf = output_string.getvalue().strip()
+    except NotImplementedError as e:
+        # log the exception
+        logging.exception(e)
+        print(f'Exception {e} on extract_text funtion from {pdfa_file}.')
+        
+    return read_pdf
+
+
+''' Returns a list of all pco numbers on the email'''
+def list_docs_from_email(pfi_pco, txt_ext):
+    if pfi_pco == 'PCOs':
+        nrpco_to_pco_re = re.compile(r'PCO CGO/(\d{4})/(\d{4})')
+        lst_pcos_to_pco = nrpco_to_pco_re.findall(txt_ext)
+        list_pcos = lst_pcos_to_pco
+
+    elif pfi_pco == 'PFIs':
+        nrpco_to_pfi_re = re.compile(r'CGO/PCO(\d{4})/(\d{4})')
+        lst_pcos_to_pfi = nrpco_to_pfi_re.findall(txt_ext)
+        list_pcos = lst_pcos_to_pfi
+    return list_pcos
+
+
+'''
+The file is an email
+looks for and gets the subject text and pco or pfis details of an email including a list of all pcos of the email.
+'''
+def prep_email_data(txt_ext):
+    
+    subj_re = re.compile(r'(PFIs|PCOs) Aprovad(a|o)s em (\d\d-\d\d-\d\d\d\d) (<> )?Lote(:)? (\d{5})( <>)? ')
+    subject = subj_re.search(txt_ext)
+    
+    if not subject is None:
+        # the doc is an email from DSL acknowledging reception of PCOs or PFIs
+        # Gets the the type of document con firmed, PCOs or PFIs fro email subject
+        pfi_pco = subject.group(1)
+        
+        # Gets lote number from email subject
+        lote_nr = subject.group(6)
+        
+        # Gets the date transmited from the email subject
+        date_transm = subject.group(3)
+        
+        #nReturns a list of all pco numbmbers on the email
+        lst_of_pcos_from_email = list_docs_from_email(pfi_pco, txt_ext)
+        
+        # Composes the complete ri file mane
+        email_full_file_name = 'RE DSL email receiving ' + pfi_pco + ' lote ' + lote_nr  + ' date ' + date_transm
+        
+        # Returns the complete archive file number
+        return pfi_pco, lst_of_pcos_from_email, email_full_file_name
+
+
+''' looks for and return the details of RI, PEI and ECI documents. '''
+def get_type_doc_details(txt_ext):
+    # looks for and gets the details of scanned RI and returns the RI details
+    ocr_ri_re = re.compile(r'RI\s?-\s?(\d{1,4})\s?/\s?(\d{4})')
+    ri_desc = ocr_ri_re.search(txt_ext)
+    if not ri_desc is None:
+        ri_details = prep_ri_data(ri_desc)
+        return ri_details
+
+    # looks for and gets the details of either scanned PEI or ECI and returns either PEI or ECI
+    ocr_peci_re = re.compile(r'(PEI|ECI) (\d{4})\s?/\s?(\d{1,4})')
+    peci_desc = ocr_peci_re.search(txt_ext)
+    if not peci_desc is None:
+        peci_details = prep_peci_data(peci_desc)
+        return peci_details
+
+    # looks for and gets the subject text and pco or pfis detail sof an email related with PFIs or PCOs sent to DSL.
+    subj_re = re.compile(r'(PFIs|PCOs) Aprovad(a|o)s em (\d\d-\d\d-\d\d\d\d) (<> )?Lote(:)? (\d{5})( <>)? ')
+    subject = subj_re.search(txt_ext)
+    if not subject is None:
+        email_details = prep_email_data(txt_ext)
+        return email_details
+
+
+''' returns the complete folter and complete file name to arquive the document. '''   
+def get_comp_filename(server, doc_type, nr, pco_year, dest_file_name):
+    if (doc_type == 'PCOs') or (doc_type == 'ECI'):
+        sub_folder = 'ORDER'
+        if doc_type == 'PCOs':
+            pco = 'PCO-'
+        elif doc_type == 'ECI':
+            pco = 'ECI-'
+    if doc_type == 'PFIs':
+        pco = 'PCO-'
+        sub_folder = 'PFI'     
+    if doc_type == 'PFI':
+        pco = 'PFIs'
+        nr = ''
+        sub_folder = ''
+    if doc_type == 'PEI':
+        pco = 'PEIs'
+        nr = ''
+        sub_folder = ''
+    if doc_type == 'RI':
+        pco = 'RIs'
+        nr = ''
+        sub_folder = ''              
+    
+    path_to_folder = server+'\\PCOs\\ORDERS ' + pco_year + '\\' + pco + nr + '\\' + sub_folder
+    path_to_file = server+'\\PCOs\\ORDERS ' + pco_year + '\\' + pco + nr + '\\' + sub_folder + '\\' + dest_file_name + '.pdf'
+    
+    return path_to_folder, path_to_file
+    
+''' copies the files to the correspondent folders. '''
+def copy_files_to_folders(ocr_file, path_to_folder, path_to_file):
+    # counter = 0
+    # if the destination folder exists    
+    if os.path.exists(path_to_folder):
+        
+        # if file exists
+        if os.path.exists(path_to_file):
+            
+            # append '(1)' to the file name
+            path_to_file = os.path.splitext(path_to_file)[0] + '(1)' + os.path.splitext(path_to_file)[1]
+        
+        # copy and rename the ocr file to the final archive folder
+        shutil.copy(ocr_file, path_to_file)
+        
+        # inform user the file is archived and ocr file was removed from scanned files
+        print(f'The original file: {os.path.basename(path_to_file)} was duly archived.')
+
+    else:
+        # Inform user the folder extracted from the file is not valid for archive.
+        print('The folder extracted from the file,\n{os.path.basename(path_to_folder)}\nis not valid for archive.')
+        
+        # Log the folder extracted from the file is not valid for archive.
+        logging.info(f'The folder extracted from the file {os.path.basename(path_to_folder)} is not valid for arquive.')
+        
